4ef3209707168192b2a1eb3b5a271a5cde98ded5
Ben Dai <65554070+b-dai@users.noreply.github.com>
2022-12-30 03:31:20 -0500

    Revert "set constants and build anim framework"

--- App.js
+++ App.js
@@ -1,45 +1,32 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ImageBackground, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
 import AnimatedSprite from 'react-native-animated-sprite';
 import knightSprite from './components/knightSprite';
 
 export default function App() {
 
-  const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
-  const leftX = SCREEN_WIDTH / 4 - 75/2;
-  const rightX = SCREEN_WIDTH / 4 * 3 - 75/2;
-  const playerHeadY = SCREEN_HEIGHT / 3 * 2;
-
   const [curAnim, setAnim] = useState('LEFT_IDLE');
-  const [curPosX, setPosX] = useState(leftX);
+  const [curLoopAnim, setLoopAnim] = useState(true);
 
   const animHandler = () => {
     setAnim('LEFT_TELEPORT');
   }
 
-  const onAnimFinished = () => {
-    if (curAnim == 'LEFT_TELEPORT')
-      setAnim('LEFT_IDLE');
-    else if (curAnim == 'RIGHT_TELEPORT')
-      setAnim('RIGHT_IDLE');
-  }
-
   return (
-    <ImageBackground source={require('./assets/back_image.png')} style={styles.container}>
+    <ImageBackground source={'./assets/back_image.png'} style={styles.container}>
       <AnimatedSprite
         sprite={knightSprite}
         animationFrameIndex={knightSprite.animationIndex(curAnim)}
-        loopAnimation={true}
+        loopAnimation={curLoopAnim}
         coordinates={{
-          top: playerHeadY,
-          left: curPosX,
+          top: 0,
+          left: 0,
         }}
         size={{
-          width: 75,
-          height: 75,
+          width: knightSprite.size.width,
+          height: knightSprite.size.width,
         }}
-        onAnimationFinish={onAnimFinished}
       />
     </ImageBackground>
   );
